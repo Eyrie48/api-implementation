b2193c381ddb6b2d646a3aa524fd3f52919dbe6b
Eyrie48 <eddiie6@hotmail.com>
2022-04-28 09:27:22 -0700

    .

--- api-implementation/vendor/composer/autoload_real.php
+++ api-implementation/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitef7f4390f723f0e02c46023be81205d1
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitef7f4390f723f0e02c46023be81205d1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitef7f4390f723f0e02c46023be81205d1', 'loadClassLoader'));
